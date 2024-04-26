7f84d788f7657804028fcef442bc1ed512246740
umitbasaak <030122073@std.izu.edu.tr>
2024-04-26 22:56:14 +0300

    Update user.go

--- models/user.go
+++ models/user.go
@@ -4,7 +4,6 @@ import "time"
 
 type User struct {
 	Userid             int       `json:"Userid"`
-	Profileimageurl    string    `json:"Profileimageurl"`
 	Username           string    `json:"Username"`
 	Password           string    `json:"Password"`
 	Fullname           string    `json:"Fullname" validate:"required,min=5,max=30"`
