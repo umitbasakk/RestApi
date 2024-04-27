de16cba361a2633616d8c5d93ade86474f07fcd8
umitbasaak <030122073@std.izu.edu.tr>
2024-04-27 17:05:18 +0300

    Update UserValidation.go

--- middleware/UserValidation.go
+++ middleware/UserValidation.go
@@ -3,11 +3,15 @@ package middleware
 import (
 	"errors"
 
+	"github.com/umitbasakk/RestApi/db"
 	"github.com/umitbasakk/RestApi/models"
 )
 
 func UserValidation(user *models.User) (bool, error) {
 
+	var users = &[]models.User{}
+
+	db.GetDb().Find(users)
 	if len(user.Fullname) < 6 || user.Fullname == "" {
 		return false, errors.New("Adınızı boş veya 6 Karakterden az olmamalıdır")
 	}
@@ -18,6 +22,15 @@ func UserValidation(user *models.User) (bool, error) {
 
 	if len(user.Password) < 8 {
 		return false, errors.New("Parolanız 8 karakterden fazla olmalıdır.")
+	}
+
+	for _, v := range *users {
+		if v.Email == user.Email {
+			return false, errors.New("Bu Kullanıcı adı başka bir kullanıcı tarafından kullanılıyor")
+		}
+		if v.Mobile == user.Mobile {
+			return false, errors.New("Bu numara başkası tarafından kullanılmaktadır.")
+		}
 	}
 
 	return true, nil
