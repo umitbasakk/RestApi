c59a3114542ed56a54c1b74013d54b984c05e07a
umitbasaak <030122073@std.izu.edu.tr>
2024-04-30 23:36:39 +0300

    Update user

--- handlers/GET/getuser.go
+++ handlers/GET/getuser.go
@@ -11,13 +11,28 @@ import (
 
 func (h *GetHandler) GetUser(c *gin.Context) {
 	id := c.Param("userid")
-	c.JSON(http.StatusOK, gin.H{"message": id})
+	user, resp := h.GetUserTokenParam(id)
+	if resp == true {
+		c.JSON(http.StatusOK, user)
+	} else {
+		c.JSON(http.StatusOK, gin.H{"message": resp})
+	}
 }
 
 func (h *GetHandler) GetUserIdParam(userID int) bool {
-	result := db.GetDb().Find(&models.User{}, "userid = ?", userID)
+	var user = models.User{}
+	result := db.GetDb().Find(&user, "userid = ?", userID)
 	if result.Row().Err() != nil {
 		return false
 	}
 	return true
+}
+
+func (h *GetHandler) GetUserTokenParam(userID string) (models.User, bool) {
+	var user = models.User{}
+	result := db.GetDb().Find(&user, "token = ?", userID)
+	if result.Row().Err() != nil {
+		return user, false
+	}
+	return user, true
 }
