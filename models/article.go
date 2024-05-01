af8540f087dd0704693678863d3611f9a9c5270d
umitbasaak <030122073@std.izu.edu.tr>
2024-05-02 00:24:35 +0300

    Update Api

--- models/article.go
+++ models/article.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type Article struct {
-	Articleid      string    `json:"Articleid" validation:"req"`
-	Imageurl       string    `json:"Imageurl" validation:"req"`
-	Title          string    `json:"Title" validation:"req"`
-	Createdtime    time.Time `json:"Createdtime" validation:"req"`
-	Author         int       `json:"Author" validation:"req"`
-	AuthorObject   ResponseUser
-	Category       int    `json:"Category" validation:"req"`
-	Articlecontent string `json:"Articlecontent" validation:"req"`
-	Comments       []Comment
+	Articleid      string       `json:"Articleid" validation:"req"`
+	Imageurl       string       `json:"Imageurl" validation:"req"`
+	Title          string       `json:"Title" validation:"req"`
+	Createdtime    time.Time    `json:"Createdtime" validation:"req"`
+	Author         int          `json:"Author" validation:"req"`
+	AuthorObject   ResponseUser `json:"AuthorObject"`
+	Category       int          `json:"Category" validation:"req"`
+	Articlecontent string       `json:"Articlecontent" validation:"req"`
+	Comments       []Comment    `json:"Comments"`
 }
