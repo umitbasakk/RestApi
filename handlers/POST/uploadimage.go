03840ec6d63078d80058f0943a7030d31ab907b0
umitbasakk <umitbasaak@gmail.com>
2024-05-02 21:17:35 +0300

    Update api

--- handlers/POST/uploadimage.go
+++ handlers/POST/uploadimage.go
@@ -3,7 +3,6 @@ package POST
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
@@ -20,58 +19,42 @@ type Picture struct {
 
 func (h *PostHandler) UploadImage(g *gin.Context) {
 	picture := Picture{}
-	err := g.Request.ParseMultipartForm(200000)
+	err := g.Request.ParseMultipartForm(10 << 20)
 	if err != nil {
 		fmt.Println(err.Error(), err)
 		return
 	}
-	formdata := g.Request.MultipartForm
-	files := formdata.File["image_file"]
+	file, handler, err := g.Request.FormFile("image_file")
 
-	for i, _ := range files {
-		file, err := files[i].Open()
-		bytes, _ := ioutil.ReadAll(file)
-		mimtype := http.DetectContentType(bytes)
-		if !strings.Contains(mimtype, "image") {
-			g.JSON(http.StatusBadRequest, "this is not an image")
-			return
-		}
-		defer file.Close()
-		if err != nil {
-			fmt.Println(err.Error())
-			g.JSON(http.StatusBadRequest, err.Error())
-			return
-		}
-
-		uniqueID := uuid.New().String()
-		filenameUnique := strings.Replace(uniqueID, "-", "", -1)
-		picture.Pictureid = filenameUnique
-		fileExt := strings.Split(files[i].Filename, ".")[1]
-		fileID := fmt.Sprintf("%s.%s", filenameUnique, fileExt)
-
-		out, err := os.Create("Images/" + fileID)
-		defer out.Close()
-		if err != nil {
-			fmt.Println("unable to create the file for writing")
-			g.JSON(http.StatusBadRequest, err.Error())
-			return
-		}
-		_, err = io.Copy(out, file)
-		if err != nil {
-			fmt.Println(err.Error())
-			g.JSON(http.StatusBadRequest, err.Error())
-			return
-		}
-		fPath := "getimage/" + fileID
-		picture.pictureurl = fPath
-		result := db.GetDb().Exec("INSERT INTO pictures VALUES($1,$2)", picture.Pictureid, picture.pictureurl)
-
-		if result.Error != nil {
-			g.JSON(http.StatusBadRequest, result.Error)
-			return
-		}
-		g.JSON(http.StatusOK, gin.H{"message": picture.Pictureid})
-
+	if err != nil {
+		fmt.Println("error")
+		return
 	}
+	defer file.Close()
+
+	uniqueID := uuid.New().String()
+	filenameUnique := strings.Replace(uniqueID, "-", "", -1)
+	picture.Pictureid = filenameUnique
+	fileExt := strings.Split(handler.Filename, ".")[1]
+	fileID := fmt.Sprintf("%s.%s", filenameUnique, fileExt)
+
+	f, err := os.OpenFile("./Images/"+fileID, os.O_WRONLY|os.O_CREATE, 0666)
+
+	if err != nil {
+		fmt.Println("error var")
+	}
+
+	defer f.Close()
+	io.Copy(f, file)
+
+	fPath := "getimage/" + fileID
+	picture.pictureurl = fPath
+	result := db.GetDb().Exec("INSERT INTO pictures VALUES($1,$2)", picture.Pictureid, picture.pictureurl)
+
+	if result.Error != nil {
+		g.JSON(http.StatusBadRequest, result.Error)
+		return
+	}
+	g.JSON(http.StatusOK, gin.H{"message": picture.Pictureid})
 
 }
