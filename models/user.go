cf3c4f58efd4da61a2048c8ac0bdf95ef4d62c56
umitbasaak <030122073@std.izu.edu.tr>
2024-05-04 18:55:23 +0300

    Update user.go

--- models/user.go
+++ models/user.go
@@ -10,7 +10,6 @@ type User struct {
 	Fullname           string    `json:"Fullname" validate:"required,min=5,max=30"`
 	Email              string    `json:"Email"  validate:"email"`
 	Mobile             string    `json:"Mobile"`
-	Gender             string    `json:"Gender"`
 	Birthday           time.Time `json:"Birthday"`
 	Followings         int       `json:"Followings"`
 	Followers          int       `json:"Followers"`
